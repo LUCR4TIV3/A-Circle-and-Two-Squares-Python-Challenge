36f733767fbaf7e51d855a4d3262875f10f4d84f
LUCR4TIV3 <141363008+LUCR4TIV3@users.noreply.github.com>
2024-09-18 17:00:53 +0100

    Add files via upload

---
+++ A Circle and Two Squares.py
@@ -0,0 +1,8 @@
+def square_areas_difference(r):
+    d = r * 2
+    areaofbsquare = d**2
+    areassquare =  ((d**2)/2)
+    total = areaofbsquare - areassquare
+    return total
+print(square_areas_difference(5))
+
